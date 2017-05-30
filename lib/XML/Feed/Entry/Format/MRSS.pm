91b87f8b5c58179430b8be5e081739409cb208d8
Joe Papperello <joe@socialflow.com>
2017-05-30 22:22:55 +0000

    media_content helper function that can properly deal with arrays versus hashes for media:content tag

--- lib/XML/Feed/Entry/Format/MRSS.pm
+++ lib/XML/Feed/Entry/Format/MRSS.pm
@@ -10,6 +10,19 @@ use XML::Feed::Content;
 
 use base qw(  XML::Feed::Entry::Format::RSS );
 
+sub media_content {
+    my $self = shift;
+    return unless ($self->{entry});
+    return unless ($self->{entry}{'media:content'});
+    if (ref($self->{entry}{'media:content'}) eq 'ARRAY') {
+        return {} unless (@{$self->{entry}{'media:content'}});
+        return $self->{entry}{'media:content'}[0];
+    }
+
+    return unless (ref($self->{entry}{'media:content'}) eq 'HASH');
+    return $self->{entry}{'media:content'};
+}
+
 
 
 1;
