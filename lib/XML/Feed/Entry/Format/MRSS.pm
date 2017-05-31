221f951286696bdc1b71bb89144da33516299f1e
Joe Papperello <joe@socialflow.com>
2017-05-31 15:42:54 +0000

    Adding media_content function

--- lib/XML/Feed/Entry/Format/MRSS.pm
+++ lib/XML/Feed/Entry/Format/MRSS.pm
@@ -1,6 +1,7 @@
 package XML::Feed::Entry::Format::MRSS;
 use strict;
 use warnings;
+use List::Util qw(first);
 
 our $VERSION = '0.53';
 
@@ -11,15 +12,26 @@ use XML::Feed::Content;
 use base qw(  XML::Feed::Entry::Format::RSS );
 
 sub media_content {
-    my $self = shift;
-    return unless ($self->{entry});
-    return unless ($self->{entry}{'media:content'});
-    if (ref($self->{entry}{'media:content'}) eq 'ARRAY') {
-        return {} unless (@{$self->{entry}{'media:content'}});
+    my ( $self, $type ) = @_;
+    return unless ( $self->{entry} );
+    return unless ( $self->{entry}{'media:content'} );
+    if ( ref( $self->{entry}{'media:content'} ) eq 'ARRAY' ) {
+        return {} unless ( @{ $self->{entry}{'media:content'} } );
+        if ($type) {
+            return first { $_->{'-medium'} eq $type }
+            @{ $self->{entry}{'media:content'} };
+        }
         return $self->{entry}{'media:content'}[0];
     }
 
-    return unless (ref($self->{entry}{'media:content'}) eq 'HASH');
+    return unless ( ref( $self->{entry}{'media:content'} ) eq 'HASH' );
+
+
+    if ($type) {
+        return
+          unless ( $self->{entry}{'media:content'}{'-medium'}
+            && $self->{entry}{'media:content'}{'-medium'} eq $type );
+    }
     return $self->{entry}{'media:content'};
 }
 
